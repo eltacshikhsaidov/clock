a838234f5abde4dcd49ce7419ada9215995f2f7c
Eltac <56788221+eltacshikhsaidov@users.noreply.github.com>
2022-04-20 00:32:02 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,26 +1,3 @@
-// let p1 = document.querySelector('.first>p');
-// let p2 = document.querySelector('.second>p');
-// let p3 = document.querySelector('.third>p');
-
-// let firstSection = document.querySelector('.first>.row');
-// let secondSection = document.querySelector('.second>.row');
-// let thirdSection = document.querySelector('.third>.row');
-
-// firstSection.addEventListener('click', function () {
-//     p1.classList.toggle('passive');
-// });
-
-// secondSection.addEventListener('click', function () {
-//     p2.classList.toggle('passive');
-
-// });
-
-// thirdSection.addEventListener('click', function () {
-//     p3.classList.toggle('passive');
-// });
-
-
-
 function string(name) {
     return document.querySelector(name);
 }
